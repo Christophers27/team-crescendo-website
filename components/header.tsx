1c14c7e2ca2d97180cb68de1ccb30a07f7d6316d
Christopher S. <christophersetiabudi@gmail.com>
2024-07-05 00:32:42 -0400

    Added underline effect in header navigation buttons

--- components/header.tsx
+++ components/header.tsx
@@ -2,16 +2,28 @@
 
 import { useMenuContext } from "@/context/menuContext";
 import { AnimatePresence, motion } from "framer-motion";
-import React from "react";
+import React, { useState } from "react";
 import { AiOutlineMenu } from "react-icons/ai";
+
+const underline = {
+  rest: {
+    backgroundColor: "transparent",
+    scaleX: 0,
+  },
+  hover: {
+    backgroundColor: "white",
+    scaleX: 1,
+  },
+};
 
 export default function Header() {
   const { showNav, toggleNav } = useMenuContext();
+  const [homeHover, setHomeHover] = useState(false);
 
   return (
     <>
-      <header className="flex flex-row p-12 relative z-[999]">
-        <div className="p-4 rounded-full fixed">
+      <header className="flex flex-row p-8 md:p-12 relative z-[999]">
+        <div className="rounded-full fixed">
           <button onClick={toggleNav}>
             <AiOutlineMenu className="text-4xl text-white" />
           </button>
@@ -20,15 +32,29 @@ export default function Header() {
       <AnimatePresence>
         {showNav && (
           <motion.div
-            className="fixed inset-0 bg-black bg-opacity-80 z-[998] flex p-40"
+            className="fixed inset-0 bg-black bg-opacity-80 z-[998] flex pt-40 px-8 md:px-12"
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
           >
-            <div className="text-center">
-              <a href="/" className="block text-white text-4xl mb-4">
-                Home
-              </a>
+            <div className="space-y-8 w-screen flex flex-col items-center md:items-start">
+              <motion.div
+                className="-space-y-4"
+                initial="rest"
+                animate="rest"
+                whileHover="hover"
+              >
+                <a
+                  href="/"
+                  className="block text-white text-4xl font-bold mb-4"
+                >
+                  HOME
+                </a>
+                <motion.div
+                  variants={underline}
+                  className="w-full h-1 origin-left"
+                />
+              </motion.div>
             </div>
           </motion.div>
         )}
