eccd69f741e32d3e263cd246d25615df83babe93
Christopher S. <christophersetiabudi@gmail.com>
2025-03-22 19:40:49 -0400

    clean start

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,5 @@
-import Image from "next/image";
-import AboutHome from "@/components/sections/aboutHome";
-import GamesIntro from "@/components/sections/gamesHome";
-import Title from "@/components/sections/title";
-import ImageDivider from "@/components/imageDivider";
+import React from "react";
 
 export default function Home() {
-  return (
-    <main className="">
-      <div className="fixed top-0 left-0 w-full h-full -z-[998] bg-[#433d60]">
-        <Image
-          src="/background.png"
-          alt="Background image"
-          fill={true}
-          objectFit="cover"
-          className="-z-[999]"
-        />
-      </div>
-      <Title />
-      <AboutHome />
-      <ImageDivider />
-      <GamesIntro />
-    </main>
-  );
+  return <div>Home</div>;
 }
